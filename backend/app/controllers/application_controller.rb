f78e08d9f25ef2f2d6a74319e25ac8df87d7a948
PYN <yxp7531540@gmail.com>
2019-07-02 09:45:52 -0500

    fixed issue

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -35,16 +35,14 @@ class ApplicationController < ActionController::Base
     def check_login
         header = cookies.signed[:jwt]
         @decoded = JsonWebToken.decode(header)
-        # byebug
+
         if @decoded != 'FAILED' 
             @current_user = User.find(@decoded[:user_id])
         end
 
         if !!@current_user 
-            # byebug
             render json: @current_user
         else
-            # byebug
             render json: { error: 'not logged in' }, status: :unauthorized
         end
     end
@@ -54,11 +52,11 @@ class ApplicationController < ActionController::Base
     end
 
     def near_events
-        # byebug
+
         @groups = Group.all.select {|group| (group.lat - params[:lat]).abs < 0.5 && (group.lng - params[:lng]).abs < 0.5 }
-        # @tournaments = Tournament.all.select {|tournament| (tournament.lat - params[:lat]).abs < 0.5 && (tournament.lng - params[:lng]).abs < 0.5 }
-        # @games = Event.all.select {|game| (game.lat - params[:lat]).abs < 0.5 && (game.lng - params[:lng]).abs < 0.5 }
 
         render json: {groups: @groups, tournament: @tournament, games: @games}, status: :ok
     end
+
+    
 end
